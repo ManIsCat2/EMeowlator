2ad0a4e1d52cfef8db92e50de2dcd809e48e5b0a
ManIsCat2 <maniscat080@gmail.com>
2026-02-12 16:11:21 +0330

    fix 2

--- src/mapper.hpp
+++ src/mapper.hpp
@@ -7,8 +7,8 @@ class PPU;
 
 class Mapper {
 public:
-    int prgBankOffset[4] = {0, 0, 0, 0};
-    int ChrBankOffset[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+    int prgBankOffset[4] = {0, 0x4000, 0, 0};
+    int ChrBankOffset[8] = {0, 0x1000, 0, 0, 0, 0, 0, 0};
     virtual ~Mapper() = default;
     virtual uint8_t cpuRead(uint16_t addr) { (void)addr; return 0; }
     virtual void cpuWrite(uint16_t addr, uint8_t value) { (void)addr; (void)value; }
